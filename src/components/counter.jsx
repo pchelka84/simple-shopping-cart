31155b3c7acea4998156b9d6d713d8f24b7aad06
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-02 15:55:37 -0400

    Adding custom styles to the Counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,17 @@ class Counter extends Component {
     count: 0
   };
 
+  style = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
