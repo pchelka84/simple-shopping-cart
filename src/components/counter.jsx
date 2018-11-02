a9a017da6c19ee8bda47997d2ada0251f4bdfab1
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-02 16:18:52 -0400

    Adding a key value to the list items

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,7 +15,7 @@ class Counter extends Component {
         <button className="btn btn-secondary btn-sm">Increment</button>
         <ul>
           {this.state.tags.map(tag => (
-            <li>{tag}</li>
+            <li key={tag}>{tag}</li>
           ))}
         </ul>
       </div>
