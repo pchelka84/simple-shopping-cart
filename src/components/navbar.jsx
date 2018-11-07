ef5e2106aadb5310faefc941b73204d7e010c4cc
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-11-07 12:40:25 -0500

    Styles changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,14 @@ import React from "react";
 const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand" href="#">
-        Shopping Cart
-        <span className="badge badge-pill badge-secondary ml-2">
-          {totalCounters}
+      <div className="container">
+        <span className="navbar-brand" href="#">
+          Shopping Cart
+          <span className="badge badge-pill badge-secondary ml-2">
+            {totalCounters}
+          </span>
         </span>
-      </span>
+      </div>
     </nav>
   );
 };
